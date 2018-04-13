c8c854d754b86d8945d68d122d607934e17fb3e3
Panu Lahtinen <pnuu+git@iki.fi>
2018-04-14 00:58:45 +0300

    Remove outliers before expanding in time, use disable cython version

--- utils/meteor_python.py
+++ utils/meteor_python.py
@@ -1,33 +1,31 @@
 import numpy as np
 
 
-def expand_in_time(meteors, times):
+def expand_in_time(x_in, y_in, times):
     """Expand detected meteors in time."""
-    labels = np.unique(meteors)
+    meteors = np.zeros(times.shape)
+    meteors[y_in, x_in] = 1
     shp = meteors.shape
-    for lbl in labels[1:]:
-        while True:
-            y_idxs, x_idxs = np.where(meteors == lbl)
-            min_t = np.min(times[y_idxs, x_idxs])
-            max_t = np.max(times[y_idxs, x_idxs])
-            num = y_idxs.size
+    i = 0
+    while True:
+        y_idxs, x_idxs = np.where(meteors == 1)
+        min_t = np.min(times[y_idxs, x_idxs])
+        max_t = np.max(times[y_idxs, x_idxs])
+        num = y_idxs.size
 
-            for y_i, x_i in zip(y_idxs, x_idxs):
-                tim = times[y_i, x_i]
-<<<<<<< HEAD
-                if tim <= min_t or tim >= max_t:
-                    for y_n in range(max(0, y_i - 3),
-                                     min(shp[0] - 1, y_i + 4)):
-                        for x_n in range(max(0, x_i - 3),
-                                         min(shp[1] - 1, x_i + 4)):
-                            if np.abs(times[y_n, x_n] - tim) < 100:
-=======
-                if tim == min_t or tim == max_t:
-                    for y_n in range(max(0, y_i - 3), min(shp[0] - 1, y_i + 4)):
-                        for x_n in range(max(0, x_i - 3), min(shp[1], x_i + 4)):
-                            if np.abs(times[y_n, x_n] - tim) < 100.:
->>>>>>> 9fbeb1f8fd7aca497760c3dbc4795e7616cc6ffb
-                                meteors[y_n, x_n] = lbl
+        for y_i, x_i in zip(y_idxs, x_idxs):
+            tim = times[y_i, x_i]
+            if tim == min_t or tim == max_t:
+                for y_n in range(max(0, y_i - 3),
+                                 min(shp[0] - 1, y_i + 4)):
+                    for x_n in range(max(0, x_i - 3),
+                                     min(shp[1] - 1, x_i + 4)):
+                        if np.abs(times[y_n, x_n] - tim) < 150:
+                            meteors[y_n, x_n] = 1
 
-            if (meteors == lbl).sum() == num:
-                break
+        if (meteors == 1).sum() == num:
+            break
+        i += 1
+    y_idxs, x_idxs = np.where(meteors == 1)
+
+    return x_idxs, y_idxs
